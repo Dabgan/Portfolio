b9cb4ef75ebe661303b527cb0a1c8479a4eca144
dabgan <gabriel.daniluk.work@gmail.com>
2020-09-10 15:53:23 +0200

    refactor: clean up code in DiagramAnimation

--- src/components/DiagramAnimation/DiagramAnimation.jsx
+++ src/components/DiagramAnimation/DiagramAnimation.jsx
@@ -2,22 +2,11 @@ import React, { useRef, useEffect } from 'react';
 import Diagram from 'assets/svg/diagram.inline.svg';
 import styled from 'styled-components';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const Wrapper = styled.div`
-    width: 100%;
-    border: 2px solid #f11b1b;
-    display: flex;
-    justify-content: flex-start;
-
     svg {
-        border: 2px solid #069cc8;
-        width: 100%;
-        /* display: flex; */
-        /* justify-content: flex-start; */
-        g #diagram {
-            border: 2px solid #f11b1b;
-            background: red;
-        }
+        /* border: 2px solid #069cc8; */
     }
 `;
 
@@ -26,147 +15,94 @@ const DiagramAnimation = () => {
 
     useEffect(() => {
         const [elements] = wrapper.current.children;
-
-        const circles = elements.getElementById('circles');
+        const circlesLine = elements.getElementById('circles');
         const person = elements.getElementById('person');
         const bars = elements.getElementById('bars');
-        const charts = elements.getElementById('charts');
         const platform = elements.getElementById('platform');
         const hop = elements.getElementById('hop');
+        const diagramLine = elements.getElementById('circleLine');
 
-        const [
-            circleLine,
-            circle1,
-            circle2,
-            circle3,
-            circle4,
-            circle5,
-        ] = circles.children;
+        gsap.registerPlugin(ScrollTrigger);
 
-        console.log(circle1, circle2, circle3, circle4, circle5, circleLine);
+        gsap.set([...circlesLine.children, person, platform, hop], {
+            autoAlpha: 0,
+        });
 
-        gsap.set(
-            [
-                ...bars.children,
-                ...circles.children,
-                ...charts.children,
-                person,
-                platform,
-                hop,
-            ],
-            {
-                autoAlpha: 0,
-            }
-        );
+        gsap.set([...bars.children, diagramLine, hop], {
+            transformOrigin: '50% 100%',
+        });
+        gsap.set(platform, { transformOrigin: '50% 50%' });
+        gsap.set([...bars.children], { scaleY: 0 });
 
-        gsap.set([...bars.children], { transformOrigin: '50% 100%' });
-        gsap.set(circleLine, { transformOrigin: '50% 100%' });
+        const pathLength = diagramLine.getTotalLength();
+        gsap.set(diagramLine, { strokeDasharray: pathLength });
 
-        const timeLine = gsap.timeline({ defaults: { ease: 'none' } });
+        const timeLine = gsap.timeline({
+            defaults: { ease: 'power1.out' },
+            scrollTrigger: { trigger: '#diagram' },
+        });
+
+        const createBarTween = (id, duration) => {
+            const myBar = [
+                bars.children[id],
+                { duration, scaleY: 1, autoAlpha: 1 },
+                'startDiagramBars',
+            ];
+            return myBar;
+        };
 
         timeLine
-            .to(platform, { duration: 1, autoAlpha: 1 })
+            .fromTo(
+                platform,
+                { scale: 0 },
+                { duration: 1, scale: 1, autoAlpha: 1 }
+            )
             .fromTo(
                 person,
                 { x: '+=200' },
-                { duration: 1, x: '-=200', autoAlpha: 1 }
+                { duration: 1, ease: 'power4.out', x: '-=201', autoAlpha: 1 }
             )
-            .addLabel('startDiagramBars', 1.5)
-            .fromTo(
-                bars.children[0],
-                { scaleY: 0 },
-                { duration: 2, scaleY: 1, autoAlpha: 1 },
-                'startDiagramBars'
-            )
+            .addLabel('startDiagramBars')
+            .to(...createBarTween(0, 1.85))
             .addLabel('finishBar1')
-            .fromTo(
-                bars.children[1],
-                { scaleY: 0 },
-                { duration: 3, scaleY: 1, autoAlpha: 1 },
-                'startDiagramBars'
-            )
+            .to(...createBarTween(1, 2.95))
             .addLabel('finishBar2')
-            .fromTo(
-                bars.children[2],
-                { scaleY: 0 },
-                { duration: 3.8, scaleY: 1, autoAlpha: 1 },
-                'startDiagramBars'
-            )
+            .to(...createBarTween(2, 3.7))
             .addLabel('finishBar3')
-            .fromTo(
-                bars.children[3],
-                { scaleY: 0 },
-                { duration: 4.5, scaleY: 1, autoAlpha: 1 },
-                'startDiagramBars'
-            )
+            .to(...createBarTween(3, 4.4))
             .addLabel('finishBar4')
+            .to(...createBarTween(4, 5.65))
+            .addLabel('finishBar5')
             .fromTo(
-                bars.children[4],
+                hop,
                 { scaleY: 0 },
-                { duration: 5.8, scaleY: 1, autoAlpha: 1 },
+                { scaleY: 1, duration: 5.65, autoAlpha: 1 },
                 'startDiagramBars'
             )
-            .addLabel('finishBar5')
-            .to(hop, {
-                duration: 1,
-                autoAlpha: 1,
-            })
-            .to(
-                circle1,
+            .fromTo(
+                diagramLine,
+                { strokeDashoffset: pathLength },
                 {
-                    duration: 1,
+                    duration: 5.4,
+                    ease: 'power1.out',
+                    strokeDashoffset: 0,
                     autoAlpha: 1,
                 },
                 'finishBar1'
-            )
-            .to(
-                circle2,
-                {
-                    duration: 1,
-                    autoAlpha: 1,
-                },
-                'finishBar2'
-            )
-            .to(
-                circle3,
-                {
-                    duration: 1,
-                    autoAlpha: 1,
-                },
-                'finishBar3'
-            )
-            .to(
-                circle4,
-                {
-                    duration: 1,
-                    autoAlpha: 1,
-                },
-                'finishBar4'
-            )
-            .to(
-                circle5,
-                {
-                    duration: 1,
-                    autoAlpha: 1,
-                },
-                'finishBar5'
-            )
-            .fromTo(
-                circleLine,
-                { scaleY: 0 },
-                {
-                    scaleY: 1,
-                    duration: 1,
-                    autoAlpha: 1,
-                },
-                'finishBar5'
-            )
-            .to(charts.children, { duration: 1.5, autoAlpha: 1, stagger: 0.5 });
+            );
+
+        [...circlesLine.children].map((circle, id) => {
+            return timeLine.to(
+                circle,
+                { delay: -0.3, duration: 1, autoAlpha: 1 },
+                `finishBar${id}`
+            );
+        });
     }, []);
 
     return (
         <Wrapper ref={wrapper}>
-            <Diagram />
+            <Diagram className="scrollTrigger" />
         </Wrapper>
     );
 };
