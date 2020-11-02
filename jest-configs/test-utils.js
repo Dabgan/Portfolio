9a8a099459d8202241412d5b8957e4d2a3555c92
dabgan <gabriel.daniluk.work@gmail.com>
2020-11-02 19:24:02 +0100

    tests: add button unit tests

--- jest-configs/test-utils.js
+++ jest-configs/test-utils.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import propTypes from 'prop-types';
 import Theme from '../src/assets/styles/theme';
 import GlobalStyle from '../src/assets/styles/GlobalStyle';
 
@@ -14,6 +15,10 @@ const AllTheProviders = ({ children }) => {
 
 const customRender = (ui, options) =>
     render(ui, { wrapper: AllTheProviders, ...options });
+
+AllTheProviders.propTypes = {
+    children: propTypes.element.isRequired,
+};
 
 export * from '@testing-library/react';
 export { customRender as render };
